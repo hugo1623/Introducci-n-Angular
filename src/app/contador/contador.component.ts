ca529d9e0047f19c657da3391514d0015c4f8e95
Hugo Luis <hugoluis.system@gmail.com>
2023-03-27 01:44:03 -0500

    Componente Contador

---
+++ src/app/contador/contador.component.ts
@@ -0,0 +1,21 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  styleUrls: ['./contador.component.css']
+})
+export class ContadorComponent {
+
+  constructor() { }
+
+
+  title = 'Contador App';
+  numero = 10;
+  base = 5;
+
+  acumulador(valor: number){
+    this.numero += valor;
+  }
+
+}
