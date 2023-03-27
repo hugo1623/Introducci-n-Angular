ac7bf4cd6705b22333a6b608a7de471735a72c63
Hugo Luis <hugoluis.system@gmail.com>
2023-03-27 02:16:51 -0500

    Cambios en el template del componente

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+  styleUrls: ['./heroe.component.css'],
 })
 export class HeroeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  nombre = 'Ironman';
+  edad = 45;
+  obtenerNombre(){
+    return `${this.nombre} - ${this.edad}`;
   }
+  get nombreCapitalizado (){
+    return this.nombre.toUpperCase();
+  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
