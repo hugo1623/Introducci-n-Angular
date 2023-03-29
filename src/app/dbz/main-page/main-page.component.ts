70a06d39e0c1885bc445422fca2519e4cce01a58
Hugo Luis <hugoluis.system@gmail.com>
2023-03-29 02:27:27 -0500

    ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
 })
-export class MainPageComponent  {
+export class MainPageComponent {
+  nuevo:Personaje = {
+    nombre : 'Truncks',
+    poder : 140000
 
-agregar(){
-  console.log("Prueba");
-}
+  };
 
+  agregar() {
+    console.log(this.nuevo);
+  }
+  cambiarNombre(event: any){
+    console.log(event.target.value);
+
+  }
 }
